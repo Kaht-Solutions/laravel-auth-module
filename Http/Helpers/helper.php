f5ac8c6cb9c3fde99a1d90a00c00864050e7bc88
Mehran NikNafs <mehran.niknafs@gmail.com>
2020-05-19 20:01:33 +0430

    Divine time from generate random string function

--- Http/Helpers/helper.php
+++ Http/Helpers/helper.php
@@ -30,6 +30,20 @@ if (!function_exists('generateRandomString')) {
         for ($i = 0; $i < $length; $i++) {
             $randomString .= $characters[rand(0, $charactersLength - 1)];
         }
+        return $randomString;
+    }
+}
+
+if (!function_exists('generateRandomStringWithTime')) {
+
+    function generateRandomStringWithTime($length = 21)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
         return $randomString . time();
     }
 }
