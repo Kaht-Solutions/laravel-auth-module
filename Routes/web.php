389d8b4bd5ca8b7234fa5ed5ff4ff76f3d9336c2
Mehran Niknafs <mehran.niknafs@gmail.com>
2021-04-07 14:26:24 +0430

    Remove routes and only keep them as Examples

--- Routes/web.php
+++ Routes/web.php
@@ -13,11 +13,12 @@
 
 Route::group(['middleware' => ['web', 'clearcache', 'guest'], 'prefix' => 'auth'], function () {
 
-    Route::get('/', 'UserAuthController@login_get');
-    Route::get('/login', 'UserAuthController@login_get')->name('login');
-    Route::post('/login', 'UserAuthController@login_post');
-    Route::get('/register', 'UserAuthController@register_get');
-    Route::post('/register', 'UserAuthController@register_post');
+    /* Examples */
+    // Route::get('/', 'UserAuthController@login_get');
+    // Route::get('/login', 'UserAuthController@login_get')->name('login');
+    // Route::post('/login', 'UserAuthController@login_post');
+    // Route::get('/register', 'UserAuthController@register_get');
+    // Route::post('/register', 'UserAuthController@register_post');
 
     // Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
     // Route::post('/password/reset', 'ResetPasswordController@reset');
@@ -28,6 +29,6 @@ Route::group(['middleware' => ['web', 'clearcache', 'guest'], 'prefix' => 'auth'
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::get('/auth/logout', 'UserAuthController@logout');
+    // Route::get('/auth/logout', 'UserAuthController@logout');
 
 });
